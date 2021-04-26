730fa4783a67b29bf775c951f6cc185e6d28a2da
adrian154 <94568a@gmail.com>
2021-04-26 11:25:03 -0700

    fix indentation

--- testprograms/99bottlesofbeer.asm
+++ testprograms/99bottlesofbeer.asm
@@ -2,23 +2,23 @@
 mov 0xfffe, re
 mov 99, r0
 loop:
-	ifeq r0, 0 mov nomore, rf
-	call printnum
+    ifeq r0, 0 mov nomore, rf
+    call printnum
     mov message0, rc
-	call printstr
+    call printstr
     call printnum
     mov message1, rc
     call printstr
-	sub r0, 1, r0
+    sub r0, 1, r0
     call printnum
     mov message2, rc
     call printstr
-	mov loop, rf
+    mov loop, rf
 nomore:
-	mov message3, rc
-	call printstr
+    mov message3, rc
+    call printstr
 done:
-	mov done, rf
+    mov done, rf
 
 ; prints number in r0
 ; only 2 digits for brevity
@@ -34,13 +34,13 @@ printsecond:
 
 ; prints string in rc
 printstr:
-	loadb rc, rd
-	ifeq rd, 0 mov doneprint, rf
-	out rd
-	add rc, 1, rc
-	mov printstr, rf
+    loadb rc, rd
+    ifeq rd, 0 mov doneprint, rf
+    out rd
+    add rc, 1, rc
+    mov printstr, rf
 doneprint:
-	popw rf
+    popw rf
 
 message0: string " bottles of beer on the wall, " byte 0
 message1: string " bottles of beer\nTake one down, pass it around, " byte 0
