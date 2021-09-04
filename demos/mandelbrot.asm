a6163542122ce93c5ee683d00135c08b9f3187c2
adrian154 <94568a@gmail.com>
2021-09-03 17:07:03 -0700

    finish mandelbrot

--- demos/mandelbrot.asm
+++ demos/mandelbrot.asm
@@ -1,84 +1,86 @@
 ; contributed by adrian
-; please excuse the sloppy register spilling
+; draws the mandelbrot set.. crudely
 
-; r0 = x
-; r1 = y
-; rc = trash
-mov 0, r0
 mov 0, r1
+yloop:
+	mov 0, r0
+	
+	xloop:
 
-; draw loop
-xloop:
-    yloop:
-        
-        ; r2, r3 = x',y' (fixed point)
-        shl r0, 3, r2 ; 3 bit shift includes implicit division by 32
+        ; r2, r3: normalized coordinates
+        ; 3 bit shift includes implicit divide by 32
+        shl r0, 2, r2
         shl r1, 3, r3
+        
+        imul r2, 2, rc, r2
+        sub r2, 382, r2
 
-        ; x' = x * 2 - 1.5
-        mul r2, 2, rc, r3 ; discard upper bits
-        sub r2, 382
+        imul r3, 2, rc, r3
+        sub r3, 255, r3
 
-        ; y' = y * 2 - 1
-        mul r3, 2, rc, r3 
-        sub r3, 255
-
-        ; r4, r5 = zimaginary, zreal
+        ; rb = whether the pixel is in the set
+        mov 1, rb
+        
+        ; r4, r5 = z components
         ; r6 = zloop counter
-        ; iterate z_{n+1} = z_n^2 + c
         mov 0, r4
         mov 0, r5
         mov 0, r6
-
-        ; flag = whether z is in the mandelbrot set
-        sf
-
         zloop:
 
-            ; r7, r8 = temp
-            ; r9, ra = z components
-            mul r4, r4, r7
-            mul r5, r5, r8
+            imul r4, r4, r7, r8 ; discard top bits
+            shr r8, 8, r8      ; drop bottom 8 bits of result
+            shl r7, 8, r7      ; move lower byte of top 16 bits to the lower word
+            or r7, r8, r7
+            ; r7 = Re(z)^2
 
-            ; calculate Im(z)
-            sub r7, r8, r9
-            add r9, r2, r9
+            imul r5, r5, r8, r9
+            shr r9, 8, r9
+            shl r8, 8, r8
+            or r8, r9, r8
+            ; r8 = Im(z)^2
 
-            ; calculate Re(z)
-            mul r4, 2, ra
-            mul ra, r5, ra
-            add ra, r3, ra
+            sub r7, r8, r7
+            add r7, r2, r7
+            ; r7 = Re(z)^2 - Im(z)^2 + x
 
-            ; move temp components back to main
-            mov r4, r9
-            mov r5, ra
+            imul r4, r5, r8, r9
+            shr r9, 8, r9
+            shl r8, 8, r8
+            or r8, r9, r8
+            shl r8, 1, r8 
+            add r8, r3, r8
+            ; r8 = 2 * Re(z) * Im(z) + y
 
-            ifl r9, 1
-            ifl ra, 1
-            mov zloop_cont, rf
+            mov r7, r4
+            mov r8, r5
 
-            ; break
-            cf
-            mov exitzloop, rf
+            ; if any component exceeds one, assume it's not bounded and leave the loop
+            ifgs r4, 255 mov 0, rb
+            ifgs r5, 255 mov 0, rb
+            ifeq rb, 0 mov zloop-exit, rf
 
-            zloop_cont:
-
+            ifeq r6, 32 mov zloop-exit, rf
             add r6, 1, r6
-            ifeq r6, 32 mov exitzloop, rf
             mov zloop, rf
 
-        exitzloop:
+        zloop-exit: 
+		
+        ; print step
+		ifeq rb, 1 out '#'
+        ifeq rb, 0 out '.'
 
-        iff out '#'
-        ifnf out ' '
+		ifeq r0, 64 mov exitxloop, rf
+		add r0, 1, r0
+		mov xloop, rf 
+	exitxloop:
+	
+    ; newline
+    out 10
 
-        add r1, 1, r1
-        ifeq r1, 64 mov exityloop, rf
-        mov yloop, rf
-
-    exityloop:
-     add r0, 1, r0
-        ifeq r0, 64 mov exitxloop, rf
-        mov xloop, rf
-exitxloop:
-    mov exitxloop, rf
+	ifeq r1, 32
+	mov done, rf
+	add r1, 1, r1
+	mov yloop, rf
+done:
+mov done, rf
